afa65849db43eb97d7958bf91a0677674acfe35d
ziyw <zywong159@gmail.com>
2023-09-25 19:46:21 -0400

    postgres example items

--- pg_docker/check-result.sh
+++ pg_docker/check-result.sh
@@ -2,3 +2,11 @@
 docker ps 
 docker exec -it <conatiner_id> bash 
 psql -U ziyan 
+
+### Table Content ### 
+ id |    title    |          content          |         created_at         
+----+-------------+---------------------------+----------------------------
+  1 | First Entry | The very first note entry | 2023-09-13 20:50:00.105541
+  2 | Entry 2     | Second try                | 2023-09-13 20:50:11.487881
+  3 | Entry three | Third time is a charm     | 2023-09-13 20:50:57.312694
+(3 rows)
